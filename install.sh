5c312a6ddd8f7c8d95835f721c54c424a53f7658
Scorpion <soleyannp@gmail.com>
2026-04-25 15:49:53 +0200

    Update install.sh to use --needed flag for packages

--- install.sh
+++ install.sh
@@ -1,30 +1,27 @@
 ## SYSTEM ##
-paru -S foot fish starship curl wget file openssl
+paru -S --needed --noconfirm foot fish starship curl wget file openssl
 
 
 ## APPS ##
-paru -S --noconfirm firefox discord
+paru -S --needed --noconfirm firefox discord
 
 
 ## GAMING (CachyOS related) ##
-paru -S --noconfirm cachyos-gaming-meta cachyos-gaming-applications
+paru -S --needed --noconfirm cachyos-gaming-meta cachyos-gaming-applications
 
 
 ## DEV ##
-paru -S --noconfirm zed neovim
+paru -S --needed --noconfirm zed neovim
 
 # Rust
-curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh # Rustup
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y # Rustup
 sudo pacman -S --needed --noconfirm webkit2gtk-4.1 base-devel appmenu-gtk-module libappindicator-gtk3 librsvg xdotool # Tauri dependencies
 
 
 ## AUR ##
-paru -S --noconfirm modrinth-app spotify
+paru -S --needed --noconfirm modrinth-app spotify
 
 
 ## CLEANUP (CachyOS related) ##
-paru -Rcs --noconfirm micro
-paru -Rcs --noconfirm evince
-paru -Rcs --noconfirm zsh
-paru -R --noconfirm alacritty meld vim
-sudo pacman -Rsn $(pacman -Qdtq)
+paru -Rcs --noconfirm alacritty evince meld micro vim zsh
+orphans=$(pacman -Qdtq) && sudo pacman -Rsn --noconfirm $orphans
