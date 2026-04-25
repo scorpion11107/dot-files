c7cb8ff9ed5f61d5c3300e70a7c8e5cd3dc32373
Scorpion <soleyannp@gmail.com>
2026-04-25 14:22:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 ## Apps ##
-paru -S firefox discord spotify
+paru -S --noconfirm firefox discord spotify
 
 ## Gaming ##
-paru -S steam modrinth-app
-sudo pacman -S cachyos-gaming-meta
+sudo pacman -S --noconfirm cachyos-gaming-meta cachyos-gaming-applications
+paru -S --noconfirm modrinth-app
 
 ## Dev ##
-paru -S git zed
+paru -S --noconfirm git zed
 
 ## Rust ##
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh # Rustup
-sudo pacman -S --needed webkit2gtk-4.1 base-devel curl wget file openssl appmenu-gtk-module libappindicator-gtk3 librsvg xdotool # Tauri dependencies
+sudo pacman -S --needed --noconfirm webkit2gtk-4.1 base-devel curl wget file openssl appmenu-gtk-module libappindicator-gtk3 librsvg xdotool # Tauri dependencies
+
+## Update and cleanup ##
+paru -R --noconfirm alacritty meld cachyos-micro-settings micro sushi evince
+sudo pacman -Rsn $(pacman -Qdtq)
+paru
