b0e1cde4e04b32a1d3cc866d8edcfc1f5825cbbb
Scorpion <soleyannp@gmail.com>
2026-04-25 15:44:53 +0200

    Refactor install.sh for organization and clarity

--- install.sh
+++ install.sh
@@ -1,18 +1,30 @@
-## Apps ##
-paru -S --noconfirm firefox discord spotify
+## SYSTEM ##
+paru -S foot fish starship curl wget file openssl
 
-## Gaming ##
-sudo pacman -S --noconfirm cachyos-gaming-meta cachyos-gaming-applications
-paru -S --noconfirm modrinth-app
 
-## Dev ##
-paru -S --noconfirm git zed
+## APPS ##
+paru -S --noconfirm firefox discord
 
-## Rust ##
+
+## GAMING (CachyOS related) ##
+paru -S --noconfirm cachyos-gaming-meta cachyos-gaming-applications
+
+
+## DEV ##
+paru -S --noconfirm zed neovim
+
+# Rust
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh # Rustup
-sudo pacman -S --needed --noconfirm webkit2gtk-4.1 base-devel curl wget file openssl appmenu-gtk-module libappindicator-gtk3 librsvg xdotool # Tauri dependencies
+sudo pacman -S --needed --noconfirm webkit2gtk-4.1 base-devel appmenu-gtk-module libappindicator-gtk3 librsvg xdotool # Tauri dependencies
 
-## Update and cleanup ##
-paru -R --noconfirm alacritty meld cachyos-micro-settings micro sushi evince
+
+## AUR ##
+paru -S --noconfirm modrinth-app spotify
+
+
+## CLEANUP (CachyOS related) ##
+paru -Rcs --noconfirm micro
+paru -Rcs --noconfirm evince
+paru -Rcs --noconfirm zsh
+paru -R --noconfirm alacritty meld vim
 sudo pacman -Rsn $(pacman -Qdtq)
-paru
