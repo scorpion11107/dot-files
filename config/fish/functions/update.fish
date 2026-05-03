a08394a56c38870bac0eeb6f4320ba312ac8aad2
Scorpion <soleyannp@gmail.com>
2026-05-03 19:06:04 +0200

    Modify update function to skip cache clearing
    Removed cache clearing step from update function.

--- config/fish/functions/update.fish
+++ config/fish/functions/update.fish
@@ -1,8 +1,6 @@
 function update
     echo "Updating system..."
     paru -Syu --noconfirm
-    echo "Clearing caches..."
-    paru -Sc --noconfirm
     echo "Removing orphans..."
     set orphans (paru -Qdtq)
     if test -n "$orphans"
