975b10202d7e8d0c02d2dd5d5e5bfa3bfb48d54c
Scorpion <soleyannp@gmail.com>
2026-05-13 17:22:08 +0200

    Delete config/fish directory

--- config/fish/functions/install-repos.fish
+++
@@ -1,9 +0,0 @@
-function install-repos
-    cd ~
-    curl https://mirror.cachyos.org/cachyos-repo.tar.xz -o cachyos-repo.tar.xz
-    tar xvf cachyos-repo.tar.xz && cd cachyos-repo
-    sudo ./cachyos-repo.sh
-    cd ~
-    rm -rf cachyos-repo
-    rm cachyos-repo.tar.xz
-end
